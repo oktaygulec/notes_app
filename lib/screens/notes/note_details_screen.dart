dd945fcdbeba3f3560ff21327a2aa5bcfd4636c6
oktaygulec <oktay.gulec@outlook.com>
2022-02-25 23:54:37 +0300

    Initiated note detail screen

---
+++ lib/screens/notes/note_details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/models/note.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return scaffold
+    return Container(
+      child: Text("hello"),
+    );
+  }
+}
