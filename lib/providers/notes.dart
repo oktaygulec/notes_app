c3edc11506604a31b615de7285894e8895fde4b7
oktaygulec <oktay.gulec@outlook.com>
2022-02-25 23:54:09 +0300

    Added getter(functions) for  - all notes  - by category  - pinned notes Changed update and delete note functions

--- lib/providers/notes.dart
+++ lib/providers/notes.dart
@@ -1,33 +1,77 @@
-import 'package:flutter/foundation.dart' show ChangeNotifier;
-import 'package:notesapp/services/storage_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../models/note.dart';
+import '/models/note.dart';
 import '/models/category.dart';
+import '/providers/categories.dart';
+import '/services/storage_manager.dart';
 
 class Notes with ChangeNotifier {
   final StorageManager _storageManager =
       StorageManager(storageName: "notes", itemName: "categories");
 
-  List<Note> getNotes(Category category) {
+  List<Note> getNotesByCategory(Category category) {
     return category.notes!.values.toList();
   }
 
-  Note? getNoteById(String taskId, Category category) {
-    return category.notes![taskId];
+  List<Note> getAllNotes(BuildContext context) {
+    final category =
+        Provider.of<Categories>(context, listen: false).items.values.toList();
+    final allNotesList = category.map((e) => e.notes!.values.toList()).toList();
+
+    List<Note> allNotes = [];
+
+    for (var notesList in allNotesList) {
+      for (var note in notesList) {
+        allNotes.add(note);
+      }
+    }
+
+    return allNotes;
   }
 
-  Future<void> newNote(Note note, Category category) async {
+  List<Note> getPinnedNotes(BuildContext context) {
+    final category =
+        Provider.of<Categories>(context, listen: false).items.values.toList();
+    final allPinnedNotesList = category
+        .map((e) => e.notes!.values.where((e) => e.isPinned).toList())
+        .toList();
+
+    List<Note> pinnedNotes = [];
+
+    for (var pinnedNotesList in allPinnedNotesList) {
+      for (var note in pinnedNotesList) {
+        pinnedNotes.add(note);
+      }
+    }
+
+    return pinnedNotes;
+  }
+
+  Future<void> newNote(Note note, BuildContext context) async {
+    final category =
+        Provider.of<Categories>(context, listen: false).items[note.categoryId];
+    if (category!.notes == null) {
+      category.notes = {};
+    }
     category.notes!.addAll({note.id: note});
     await _storageManager.updateItem(category);
+    notifyListeners();
   }
 
-  Future<void> updateNote(Note note, Category category) async {
-    category.notes![note.id] = note;
+  Future<void> updateNote(Note note, BuildContext context) async {
+    final category =
+        Provider.of<Categories>(context, listen: false).items[note.categoryId];
+    category!.notes![note.id] = note;
     await _storageManager.updateItem(category);
+    notifyListeners();
   }
 
-  Future<void> deleteNote(String taskId, Category category) async {
-    category.notes!.removeWhere((key, value) => key == taskId);
+  Future<void> deleteNote(Note note, BuildContext context) async {
+    final category =
+        Provider.of<Categories>(context, listen: false).items[note.categoryId];
+    category!.notes!.removeWhere((key, value) => key == note.id);
     await _storageManager.updateItem(category);
+    notifyListeners();
   }
 }
