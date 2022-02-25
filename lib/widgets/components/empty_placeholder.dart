8530fcd14de817b509d15b832231cda1408ee75c
oktaygulec <oktay.gulec@outlook.com>
2022-02-25 23:54:57 +0300

    Created an empty placeholder view

---
+++ lib/widgets/components/empty_placeholder.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/widgets/components/icon_text.dart';
+import '/enums.dart';
+import '/widgets/components/custom_text.dart';
+
+class EmptyPlaceholder extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final String buttonText;
+  final VoidCallback? buttonOnPress;
+  const EmptyPlaceholder({
+    Key? key,
+    required this.text,
+    required this.icon,
+    required this.buttonText,
+    required this.buttonOnPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: TextType.emptyPlaceholder.style!.color,
+            size: 128,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomText(
+            text,
+            alignment: TextAlign.center,
+            textType: TextType.emptyPlaceholder,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          ElevatedButton(
+            child: IconText(
+              icon: const Icon(Icons.add),
+              text: buttonText.toUpperCase(),
+              textType: TextType.primaryButton,
+              iconPosition: IconPosition.left,
+              isStretched: false,
+            ),
+            onPressed: buttonOnPress,
+          ),
+        ],
+      ),
+    );
+  }
+}
